8b3340f7fd07dc8a12f83f2af71691027646f623
Utkarsh Chaturvedi <utkarshc0@gmail.com>
2024-03-01 19:30:30 +0530

    Adding uncommitted files

---
+++ lib/Constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const String APP_NAME = "Simple Debit Manager";
+  static const String INTRODUCTORY_TEXT ="An application meant for a clean view of your debits in a systematic manner.";
+}
---
+++ lib/view/IntroductionView.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// View class to enable google login
+// View class should also give a brief about how the app will worl
+class IntroductionView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  }
+}
